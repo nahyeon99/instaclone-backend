f865b851d0fa9c0f6d48c7541dec84ba3f495589
NaHyeon <nahyeonee99@gmail.com>
2021-05-26 19:56:49 +0900

    #4.2 Create Account part Three

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import client from "../client";
 
 export default {
@@ -16,6 +17,17 @@ export default {
               email,
             },
           ],
+        },
+      });
+
+      const uglyPassword = await bcrypt.hash(password, 10);
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
         },
       });
 
