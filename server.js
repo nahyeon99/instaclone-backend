e48b9cf132e366e879a2982904143bf618795028
NaHyeon <nahyeonee99@gmail.com>
2021-05-28 23:21:02 +0900

    #4.9 authentication part Two

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import schema from "./schema";
 
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjIyMTgzNzUzfQ.Wc0lVcaI46Rpv2YVWNL_OFt4lZ52YrSXpDOn0pkZEq0",
+  },
 });
 
 const PORT = process.env.PORT;
